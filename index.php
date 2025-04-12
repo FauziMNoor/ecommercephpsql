d99bc1a14478a72b77746b714c3f863638679ab6
Fauzi M. Noor <fauzimnoor90@gmail.com>
2025-04-13 01:03:43 +0700

    first commit

--- index.php
+++ index.php
@@ -4,13 +4,8 @@ if(!isset($_SESSION['login'])){
     header("location:login.php");
     exit;
 }
-?>
-<a href="logout.php">Logout</a>
-<link rel="stylesheet" type="text/css" href="style.css">
 
-<?php
 include 'koneksi.php';
-
 
 // Ambil filter kategori (jika ada)
 $kategori = isset($_GET['kategori']) ? $_GET['kategori'] : '';
@@ -27,6 +22,13 @@ if($kategori != ''){
 <html>
 <head>
     <title>Daftar Produk</title>
+
+    <!-- CSS Custom -->
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <!-- Font Awesome CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
     <style>
         .produk {
             border: 1px solid #ddd;
@@ -41,17 +43,28 @@ if($kategori != ''){
         img {
             border-radius: 5px;
         }
+        a i {
+            font-size: 18px;
+            color: #555;
+            margin: 0 5px;
+            transition: 0.3s;
+        }
+        a i:hover {
+            color: #ff4081;
+            transform: scale(1.2);
+        }
     </style>
 </head>
 <body>
 
 <h1>Daftar Produk</h1>
 
-<!-- Link tambah produk -->
+<a href="logout.php">Logout</a> |
+<a href="keranjang.php"><i class="fas fa-shopping-cart"></i> Keranjang</a> |
 <a href="tambah.php">+ Tambah Produk Baru</a>
 
-<!-- Form Filter -->
 <h2>Filter Produk</h2>
+
 <form method="GET" action="">
     <select name="kategori">
         <option value="">-- Semua Kategori --</option>
@@ -64,18 +77,27 @@ if($kategori != ''){
 <!-- Looping Produk -->
 <?php while($data = mysqli_fetch_assoc($query)) { ?>
     <div class="produk">
-        <a href="detail.php?id=<?php echo $data['id']; ?>">Lihat Detail</a> | 
-        <a href="edit.php?id=<?php echo $data['id']; ?>">Edit</a> | 
-        <a href="hapus.php?id=<?php echo $data['id']; ?>" onclick="return confirm('Yakin mau hapus?')">Hapus</a>
-
         <img src="gambar/<?php echo $data['gambar']; ?>" width="150" height="150">
         <h3><?php echo $data['nama_produk']; ?></h3>
         <p>Kategori: <?php echo $data['kategori']; ?></p>
         <p>Harga: Rp <?php echo number_format($data['harga']); ?></p>
 
-        <!-- Link Edit & Hapus -->
-        <a href="edit.php?id=<?php echo $data['id']; ?>">Edit</a> | 
-        <a href="hapus.php?id=<?php echo $data['id']; ?>" onclick="return confirm('Yakin mau hapus?')">Hapus</a>
+        <!-- Icon Action -->
+        <a href="detail.php?id=<?php echo $data['id']; ?>" title="Detail">
+            <i class="fas fa-eye"></i>
+        </a>
+
+        <a href="cart.php?id=<?php echo $data['id']; ?>" title="Tambah ke Keranjang">
+            <i class="fas fa-cart-plus"></i>
+        </a>
+
+        <a href="edit.php?id=<?php echo $data['id']; ?>" title="Edit">
+            <i class="fas fa-pen"></i>
+        </a>
+
+        <a href="hapus.php?id=<?php echo $data['id']; ?>" onclick="return confirm('Yakin mau hapus?')" title="Hapus">
+            <i class="fas fa-trash"></i>
+        </a>
     </div>
 <?php } ?>
 
